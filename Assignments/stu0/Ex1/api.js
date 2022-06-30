5619dd1c1065ac12b33a6db3e5b72b9fac5e00b3
Martin Burolla <mburolla@xpanxion.com>
2022-06-30 12:04:46 -0400

    update

--- Assignments/stu0/Ex1/api.js
+++ Assignments/stu0/Ex1/api.js
@@ -7,7 +7,7 @@
 
 const cors = require('cors');
 const express = require('express');
-const { getPerson } = require('./data-access');
+const dataAccess = require('./data-access');
 
 const PORT = 5152;
 const app = express();
@@ -27,36 +27,10 @@ app.use(cors());
 //
 
 app.get('/persons/:id', cors(corsOptions), async (req, res) => { 
-    let person = await getPerson(req.params['id'])
+const da = require('./data-access');
+    let person = await dataAccess.getPerson(req.params['id'])
+    // let body = req.body;
     res.send(person);
-});
-
-//
-// POST /persons
-//
-// Body:
-// {
-//    id: <number>,
-//    name: <string>,
-//    age: <number>
-// }
-//
-
-app.post('/persons', cors(corsOptions), (req, res) => {
-    personArray.push(req.body)
-    console.log(personArray)
-    res.send(req.body);
-});
-
-//
-// DELETE /persons/:id
-//
-
-app.delete('/persons/:id', cors(corsOptions), (req, res) => {
-    let index = personArray.findIndex(p => p.id === parseInt(req.params['id']))
-    personArray.splice(index, 1)
-    console.log(personArray)
-    res.send("Ok");
 });
 
 app.listen(PORT, () => {
