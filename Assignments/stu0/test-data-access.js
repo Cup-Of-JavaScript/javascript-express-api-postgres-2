1a17c3949845937dd3990b107e00b02ece9eaaaa
Martin Burolla <mburolla@xpanxion.com>
2022-07-15 15:02:12 -0400

    update

--- Assignments/stu0/test-data-access.js
+++ Assignments/stu0/test-data-access.js
@@ -1,14 +1,6 @@
 const dataAccess = require('./data-access');
 
 const main = async () => {
-    let person  = {
-        personTypeId: 1,
-        bookStoreId: 1,
-        firstName: "joe",
-        lastName: "smith",
-        dob: "8/29/1970"
-    }
-
     let r = await dataAccess.insertTransaction(3,1,600.00, '7/15/2022')
     console.log(r)
     process.exit()
