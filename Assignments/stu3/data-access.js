2bf4d16b03302676ee0c31695771420f85e3f386
Antionette Van <avan@xpanxion.com>
2022-07-18 14:16:34 -0700

    update

--- Assignments/stu3/data-access.js
+++ Assignments/stu3/data-access.js
@@ -1,13 +1,29 @@
-const GET_ACCOUNT_TYPE = 'select * from account_type'
+const GET_ACCOUNT_TYPES = 'select * from account_type'
+const GET_TRANSACTION_TYPES = 'select * from transaction_type'
 
 const { pool } = require("../../postgres-pool");
 const currencyFormatter = require('currency-formatter');
 
+// EX 1
 
-exports.getAccoutTypes = async () => {
+exports.getAccountTypes = async () => {
     let retval = null;
     try {
-        let r = await pool.query(GET_ACCOUNT_TYPE)
+        let r = await pool.query(GET_ACCOUNT_TYPES)
+        retval = r.rows
+    } catch (err) {
+        console.error(err);
+    }
+    return retval;
+}
+
+
+// EX 2
+
+exports.getTransactionTypes = async () => {
+    let retval = null;
+    try {
+        let r = await pool.query(GET_TRANSACTION_TYPES)
         retval = r.rows
     } catch (err) {
         console.error(err);
