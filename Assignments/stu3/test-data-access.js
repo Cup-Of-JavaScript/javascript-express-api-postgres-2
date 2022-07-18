029b757a1c27a8d54484bcb6c15505f76b331434
Antionette Van <avan@xpanxion.com>
2022-07-18 16:28:35 -0700

    update

--- Assignments/stu3/test-data-access.js
+++ Assignments/stu3/test-data-access.js
@@ -6,7 +6,7 @@
 const dataAccess = require('./data-access');
 
 const main = async () => {
-    let r = await dataAccess.getTransactionTypes()
+    let r = await dataAccess.getUsersByDob(1972)
     console.log(r)
     process.exit()
 }
