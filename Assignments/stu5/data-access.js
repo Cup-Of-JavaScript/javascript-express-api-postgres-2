d3df13395d6f66b221a3b70acdceaad8b7ee8a8f
Diane Tewah <dtewah@xpanxion.com>
2022-07-18 12:40:29 -0400

    Ex. 2 Get Transaction Types

--- Assignments/stu5/data-access.js
+++ Assignments/stu5/data-access.js
@@ -8,11 +8,23 @@ const { pool } = require("../../postgres-pool");
 const currencyFormatter = require('currency-formatter');
 
 const GET_ACCOUNT_TYPES = 'select * from account_type'
+const GET_TRANSACTION_TYPES = 'select * from transaction_type'
 
 module.exports.getAccoutTypes = async () => {
     let retval = null;
     try {
         let r = await pool.query(GET_ACCOUNT_TYPES);
+        retval = r.rows;
+    } catch (err) {
+        console.error(err);
+    }
+    return retval;
+}
+
+module.exports.getTransactionTypes = async () => {
+    let retval = null;
+    try {
+        let r = await pool.query(GET_TRANSACTION_TYPES);
         retval = r.rows;
     } catch (err) {
         console.error(err);
