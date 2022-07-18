deb7ebe69ccfcaf7a8fdc2a98e7abb6b6f25e747
Sherika Fayson <sfayson@xpanxion.com>
2022-07-18 11:52:09 -0400

    Ex. 2 Get Transaction Types

--- Assignments/stu10/test-data-access.js
+++ Assignments/stu10/test-data-access.js
@@ -6,7 +6,7 @@
 const dataAccess = require('./data-access');
 
 const main = async () => {
-    let r = await dataAccess.getAccoutTypes()
+    let r = await dataAccess.getTransactionTypes()
     console.log(r)
     process.exit()
 }
