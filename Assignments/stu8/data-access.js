94e0ed75b89da0e7bc8fc009d2747cebdd200b43
Michelle Gilyeat <mgilyeat@xpanxion.com>
2022-07-18 11:19:29 -0400

    Ex 2 Transaction Types

--- Assignments/stu8/data-access.js
+++ Assignments/stu8/data-access.js
@@ -6,6 +6,9 @@
 
 const GET_ACCOUNT_TYPE = `select * from account_type`
 
+const GET_TRANSACTION_TYPE = `select * from transaction_type`
+
+
 
 const { pool } = require("../../postgres-pool");
 const currencyFormatter = require('currency-formatter');
@@ -14,6 +17,17 @@ exports.getAccountTypes = async () => {
     let retval = null;
     try {
         let r = await pool.query(GET_ACCOUNT_TYPE);
+        retval = r.rows;
+    } catch (err) {
+        console.error(err);
+    }
+    return retval;
+}
+
+exports.getTransactionTypes = async () => {
+    let retval = null;
+    try {
+        let r = await pool.query(GET_TRANSACTION_TYPE);
         retval = r.rows;
     } catch (err) {
         console.error(err);
