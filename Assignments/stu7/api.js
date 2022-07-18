5b0fc67511fe60a2a6c7acda22e97ff18682c282
Kelsey Perkins <kperkins@xpanxion.com>
2022-07-18 07:38:48 -0700

    getting started

--- Assignments/stu7/api.js
+++ Assignments/stu7/api.js
@@ -21,9 +21,21 @@ app.use(express.json());
 app.use(express.urlencoded());
 app.use(cors());
 
-//
-// API ENDPOINTS GO HERE...
-//
+//ex.1
+app.get('/ex1/persons', cors(corsOptions), async (req, res) => { 
+    // Parsing...
+    // const id = req.params['id'];                 // Parse the path params from URL (e.g. /persons/1)
+    // const queryParam1 = req.query['personType']  // Parse the query string from URL (e.g. ?personType=manager)
+    // const body = req.body;                       // Parse the the body from the request
+    
+    // Data access & business logic...
+    // const result = await dataAccess.<YOUR FUNCTION HERE>
+    
+    // Response...
+    // res.status(404); // 201, 400, 403, etc.
+    // res.send(<YOUR OBJECT HERE>);
+});
+
 
 app.listen(PORT, () => {
     console.log(`Bookstore API is running on port: ${PORT}`);
