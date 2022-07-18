f828ed0d3751730fd9bd998cc0ac1820d8783aaf
Martin Burolla <mburolla@xpanxion.com>
2022-07-18 07:40:06 -0400

    update

--- Assignments/stu0/test-data-access.js
+++
@@ -1,9 +0,0 @@
-const dataAccess = require('./data-access');
-
-const main = async () => {
-    let r = await dataAccess.getAccountBalanceForAccountId(1);
-    console.log(r)
-    process.exit()
-}
-
-main()
